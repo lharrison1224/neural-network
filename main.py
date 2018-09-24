4f5b8cdbc44c8499e488f6c59b2bfd4653cd5449
Logan Harrison <lkh6yb@mail.missouri.edu>
2018-09-24 17:32:46 -0500

    calculate deltas for backprop

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ LEARNING_RATE = 0.7
 MOMENTUM = 0.3
 ACTIVATION_SLOPE_PARAM = 1
 ACTIVATION_FUNCTION = "SIGMOID"
+NUM_LAYERS = 2
+NUM_EPOCHS = 1
 
 
 def main():
@@ -57,7 +59,7 @@ def main():
             training_data.append(new_row)
 
     # loop for number of epochs
-    for epoch in range(1):
+    for epoch in range(NUM_EPOCHS):
 
         # loop over pieces of data in training set
         for k in range(len(training_data)):
@@ -76,7 +78,7 @@ def main():
             phi_v = []
 
             # loop over layers
-            for layer in range(2):
+            for layer in range(NUM_LAYERS):
 
                 # add placeholder for next layer
                 phi_v.append([])
@@ -104,13 +106,39 @@ def main():
                     # store the phi(v) for the node
                     phi_v[layer].append(phi(v))
 
-                # END node loop
-            print(phi_v)
-            # END layer loop
+                    # END node loop
 
-        # END item loop
+                # END layer loop
 
-    # END epcoh loop
+            # loop over output nodes
+            errors = []
+            for output_node in range(len(phi_v[NUM_LAYERS-1])):
+                errors.append(
+                    expected_outputs[output_node] - phi_v[NUM_LAYERS-1][output_node])
+
+            deltas = [[] for _ in range(NUM_LAYERS)]
+            # calculating deltas
+            for layer in range(NUM_LAYERS-1, -1, -1):
+
+                # loop over nodes in the layer
+                for node in range(len(phi_v[layer])):
+                    # output layer
+                    if layer is NUM_LAYERS-1:
+                        deltas[layer].append(
+                            errors[node] * phi_prime(phi_v[layer][node]))
+
+                    # hidden layer
+                    else:
+                        sum_delta_times_weights = 0
+                        for next_node in range(len(phi_v[layer+1])):
+                            sum_delta_times_weights += deltas[layer +
+                                                              1][next_node] * weights[layer+1][next_node][node]
+                        deltas[layer].append(
+                            phi_prime(phi_v[layer][node]) * sum_delta_times_weights)
+
+            # END item loop
+
+        # END epcoh loop
 
 
 def phi(v):
@@ -118,9 +146,9 @@ def phi(v):
         return 1/(1 + math.exp(-1 * ACTIVATION_SLOPE_PARAM * v))
 
 
-def phi_prime(v):
+def phi_prime(phi_v):
     if ACTIVATION_FUNCTION is "SIGMOID":
-        return ACTIVATION_SLOPE_PARAM * (phi(v)) * (1-phi(v))
+        return ACTIVATION_SLOPE_PARAM * phi_v * (1-phi_v)
 
 
 if __name__ == "__main__":
